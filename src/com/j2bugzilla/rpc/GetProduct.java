957ad475f4e44bc3a9bcf64072ac82f4fb471e14
TomRK1089 <TomRK1089@localhost>
2012-02-02 17:50:14 -0500

    Although the Webservice spec lists a 'names' parameter, this only was added for Bugzilla version 4.2, which does not exist yet. There is a release candidate, but no stable release. Thus products can only be searched via ID. Compounding my confusion was the fact that my one and only product on the test installation had an ID of 2, not 1 as I expected. I assume the test product Bugzilla 'helpfully' creates for you on install takes ID #1.

--- src/com/j2bugzilla/rpc/GetProduct.java
+++ src/com/j2bugzilla/rpc/GetProduct.java
@@ -25,10 +25,6 @@ public class GetProduct implements BugzillaMethod {
 	
 	private Map<Object, Object> hash = Collections.emptyMap();
 	
-	public GetProduct(String name) {
-		params.put("name", new String[] { name });
-	}
-	
 	public GetProduct(int id) {
 		params.put("ids", new Integer[] { id });
 	}
@@ -37,12 +33,11 @@ public class GetProduct implements BugzillaMethod {
 		Object products = hash.get("products");
 		if(products == null) { return null; }
 		
+		Object[] arr = (Object[])products;
+		if(arr.length == 0) { return null; }
+		
 		@SuppressWarnings("unchecked")//Cast to form specified by webservice
-		Map<Object, Object>[] arr = (Map<Object, Object>[])products;
-		if(arr.length == 0) { return null; }//Defend against empty results
-		
-		Map<Object, Object> prodMap = arr[0];
-		
+		Map<Object, Object> prodMap = (Map<Object, Object>)arr[0];
 		Product product = new Product((Integer)prodMap.get("id"), (String)prodMap.get("name"));
 		String desc = (String)prodMap.get("description");
 		product.setDescription(desc);
@@ -64,5 +59,4 @@ public class GetProduct implements BugzillaMethod {
 	public String getMethodName() {
 		return METHOD_NAME;
 	}
-
 }
