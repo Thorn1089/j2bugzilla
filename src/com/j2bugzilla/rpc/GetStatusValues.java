1c503b6ba61316f059d70418f4dd0b3d90363eb9
TomRK1089 <TomRK1089@localhost>
2012-02-03 15:22:57 -0500

    Added Javadoc.

--- src/com/j2bugzilla/rpc/GetStatusValues.java
+++ src/com/j2bugzilla/rpc/GetStatusValues.java
@@ -8,12 +8,25 @@ import java.util.Set;
 
 import com.j2bugzilla.base.BugzillaMethod;
 
+/**
+ * The {@code GetStatusValues} class is a helper method which wraps the {@code Bug.fields} webservice call, to
+ * provide clients a list of legal status values from their installation of Bugzilla. Due to the workflow change
+ * in Bugzilla 4.0, different installations may have different values. Thus, this method allows clients to
+ * work against a predetermined correct set.
+ * 
+ * @author Tom
+ *
+ */
 public class GetStatusValues implements BugzillaMethod {
 
 	private static final String METHOD_NAME = "Bug.fields";
 	
 	private Set<String> legalValues = Collections.emptySet();
 	
+	/**
+	 * Returns a {@code Set} of {@code Strings} representing the legal values for the status of a {@link Bug}.
+	 * @return A set of values for a bug's status field.
+	 */
 	public Set<String> getLegalValues() {
 		return Collections.unmodifiableSet(legalValues);
 	}
@@ -38,7 +51,7 @@ public class GetStatusValues implements BugzillaMethod {
 	@Override
 	public Map<Object, Object> getParameterMap() {
 		Map<Object, Object> params = new HashMap<Object, Object>();
-		params.put("names", new String[] { "status" });
+		params.put("names", new String[] { "bug_status" });
 		return Collections.unmodifiableMap(params);
 	}
 
@@ -46,5 +59,4 @@ public class GetStatusValues implements BugzillaMethod {
 	public String getMethodName() {
 		return METHOD_NAME;
 	}
-
 }
