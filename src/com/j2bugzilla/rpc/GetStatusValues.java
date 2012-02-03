08c5c92e3e44bf7b2d4440273ab47bd966a10ac8
TomRK1089 <TomRK1089@localhost>
2012-02-03 15:17:07 -0500

    Fleshed out GetStatusValues implementation.

--- src/com/j2bugzilla/rpc/GetStatusValues.java
+++ src/com/j2bugzilla/rpc/GetStatusValues.java
@@ -2,7 +2,9 @@ package com.j2bugzilla.rpc;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.j2bugzilla.base.BugzillaMethod;
 
@@ -10,10 +12,27 @@ public class GetStatusValues implements BugzillaMethod {
 
 	private static final String METHOD_NAME = "Bug.fields";
 	
+	private Set<String> legalValues = Collections.emptySet();
+	
+	public Set<String> getLegalValues() {
+		return Collections.unmodifiableSet(legalValues);
+	}
+	
 	@Override
 	public void setResultMap(Map<Object, Object> hash) {
-		// TODO Auto-generated method stub
-
+		legalValues = new HashSet<String>();
+		
+		Object[] array = (Object[]) hash.get("fields");
+		@SuppressWarnings("unchecked")//Cast to structure defined by webservice
+		Map<Object, Object> fields = (Map<Object, Object>) array[0];
+		
+		Object[] values = (Object[]) fields.get("values");
+		for(Object obj : values) {
+			@SuppressWarnings("unchecked")//Cast to structure defined by webservice
+			Map<Object, Object> map = (Map<Object, Object>) obj;
+			String name = (String) map.get("name");
+			legalValues.add(name);
+		}
 	}
 
 	@Override
