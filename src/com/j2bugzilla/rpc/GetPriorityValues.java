0a354048dcb86e8b5bc15c884eac74387e835f1c
TomRK1089 <TomRK1089@localhost>
2012-02-03 17:44:40 -0500

    Added stub for GetPriorityValues, to retrieve a set of legal priority values from the Bugzilla installation.

---
+++ src/com/j2bugzilla/rpc/GetPriorityValues.java
@@ -0,0 +1,27 @@
+package com.j2bugzilla.rpc;
+
+import java.util.Map;
+
+import com.j2bugzilla.base.BugzillaMethod;
+
+public class GetPriorityValues implements BugzillaMethod {
+
+	@Override
+	public void setResultMap(Map<Object, Object> hash) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<Object, Object> getParameterMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMethodName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
