a8b83242943a5636dace3a0d5d60f4c98e2af9fe
TomRK1089 <TomRK1089@localhost>
2012-02-03 15:11:06 -0500

    Added method name and parameter map.

--- src/com/j2bugzilla/rpc/GetStatusValues.java
+++ src/com/j2bugzilla/rpc/GetStatusValues.java
@@ -1,11 +1,15 @@
 package com.j2bugzilla.rpc;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.j2bugzilla.base.BugzillaMethod;
 
 public class GetStatusValues implements BugzillaMethod {
 
+	private static final String METHOD_NAME = "Bug.fields";
+	
 	@Override
 	public void setResultMap(Map<Object, Object> hash) {
 		// TODO Auto-generated method stub
@@ -14,14 +18,14 @@ public class GetStatusValues implements BugzillaMethod {
 
 	@Override
 	public Map<Object, Object> getParameterMap() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Object, Object> params = new HashMap<Object, Object>();
+		params.put("names", new String[] { "status" });
+		return Collections.unmodifiableMap(params);
 	}
 
 	@Override
 	public String getMethodName() {
-		// TODO Auto-generated method stub
-		return null;
+		return METHOD_NAME;
 	}
 
 }
