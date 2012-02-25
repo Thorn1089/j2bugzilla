f1ffe381101c6a7211c1230d0f5ab4d978f1e34a
TomRK1089 <TomRK1089@localhost>
2012-02-25 11:45:02 -0500

    Fleshed out GetPriorityValues class.

--- src/com/j2bugzilla/rpc/GetPriorityValues.java
+++ src/com/j2bugzilla/rpc/GetPriorityValues.java
@@ -1,27 +1,54 @@
 package com.j2bugzilla.rpc;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.j2bugzilla.base.BugzillaMethod;
 
 public class GetPriorityValues implements BugzillaMethod {
 
+private static final String METHOD_NAME = "Bug.fields";
+	
+	private Set<String> legalValues = Collections.emptySet();
+	
+	/**
+	 * Returns a {@code Set} of {@code Strings} representing the legal values for the status of a {@link Bug}.
+	 * @return A set of values for a bug's status field.
+	 */
+	public Set<String> getLegalValues() {
+		return Collections.unmodifiableSet(legalValues);
+	}
+	
 	@Override
 	public void setResultMap(Map<Object, Object> hash) {
-		// TODO Auto-generated method stub
-
+		legalValues = new HashSet<String>();
+		
+		Object[] array = (Object[]) hash.get("fields");
+		@SuppressWarnings("unchecked")//Cast to structure defined by webservice
+		Map<Object, Object> fields = (Map<Object, Object>) array[0];
+		
+		Object[] values = (Object[]) fields.get("values");
+		for(Object obj : values) {
+			@SuppressWarnings("unchecked")//Cast to structure defined by webservice
+			Map<Object, Object> map = (Map<Object, Object>) obj;
+			String name = (String) map.get("name");
+			legalValues.add(name);
+		}
 	}
 
 	@Override
 	public Map<Object, Object> getParameterMap() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Object, Object> params = new HashMap<Object, Object>();
+		params.put("names", new String[] { "priority" });
+		return Collections.unmodifiableMap(params);
 	}
 
 	@Override
 	public String getMethodName() {
-		// TODO Auto-generated method stub
-		return null;
+		return METHOD_NAME;
 	}
 
 }
