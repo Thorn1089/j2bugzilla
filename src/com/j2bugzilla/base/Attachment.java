09d4d275dc4708722c615264d6099ed161091c88
TomRK1089 <TomRK1089@localhost>
2012-02-27 18:00:30 -0500

    Added stub for Attachment class.

---
+++ src/com/j2bugzilla/base/Attachment.java
@@ -0,0 +1,13 @@
+package com.j2bugzilla.base;
+
+/**
+ * The {@code Attachment} class defines certain shared properties of files attached to a bug report.
+ * Subclasses of {@code Attachment} may provide additional fields and methods to display and manipulate
+ * the specific attachment data.
+ * 
+ * @author Tom
+ *
+ */
+public abstract class Attachment {
+
+}
