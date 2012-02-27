4b42d9f6e15f05f7d6f366809e1e2e8e3c1b87a0
TomRK1089 <TomRK1089@localhost>
2012-02-27 18:56:01 -0500

    Added stub for AttachmentFactory.

---
+++ src/com/j2bugzilla/base/AttachmentFactory.java
@@ -0,0 +1,13 @@
+package com.j2bugzilla.base;
+
+/**
+ * The {@code AttachmentFactory} is responsible for creating new {@link Attachment} objects based off of
+ * data from either a Bugzilla installation or user input. It is responsible for determining the correct
+ * {@code Attachment} subtype to instantiate.
+ * 
+ * @author Tom
+ *
+ */
+public class AttachmentFactory {
+
+}
