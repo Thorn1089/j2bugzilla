f8e1667b76c6f3a5037b8703dee0a801da59798f
TomRK1089 <TomRK1089@localhost>
2012-02-27 23:57:32 -0500

    Added constructors and overloads.

--- src/com/j2bugzilla/rpc/AddAttachment.java
+++ src/com/j2bugzilla/rpc/AddAttachment.java
@@ -4,7 +4,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.j2bugzilla.base.Attachment;
+import com.j2bugzilla.base.Bug;
 import com.j2bugzilla.base.BugzillaMethod;
+import com.j2bugzilla.base.Comment;
 
 public class AddAttachment implements BugzillaMethod {
 
@@ -12,6 +15,27 @@ public class AddAttachment implements BugzillaMethod {
 	
 	private Map<Object, Object> params = new HashMap<Object, Object>();
 	private Map<Object, Object> hash = new HashMap<Object, Object>();
+	
+	public AddAttachment(Attachment attachment, int id) {
+		params.put("ids", id);
+		params.put("data", attachment.getRawData());
+		params.put("file_name", attachment.getFileName());
+		params.put("summary", attachment.getSummary());
+		params.put("content_type", attachment.getMIMEType());
+	}
+	
+	public AddAttachment(Attachment attachment, Bug bug) {
+		this(attachment, bug.getID());
+	}
+	
+	public AddAttachment(Attachment attachment, Bug bug, String comment) {
+		this(attachment, bug);
+		params.put("comment", comment);
+	}
+	
+	public AddAttachment(Attachment attachment, Bug bug, Comment comment) {
+		this(attachment, bug, comment.getText());
+	}
 	
 	@Override
 	public void setResultMap(Map<Object, Object> hash) {
