deb6c6e6c2456d27a6b59c65a7feb90745b2b4e4
TomRK1089 <TomRK1089@localhost>
2012-02-27 20:49:09 -0500

    Added Javadoc to setters.

--- src/com/j2bugzilla/base/AttachmentFactory.java
+++ src/com/j2bugzilla/base/AttachmentFactory.java
@@ -42,6 +42,11 @@ public class AttachmentFactory {
 
 	private Date modified;
 	
+	/**
+	 * Tells the {@link AttachmentFactory} to begin building a new {@link Attachment}. This method must
+	 * be called before any of the {@code set} methods, and before {@link #createAttachment() createAttachment()}.
+	 * @return A reference to the current factory, so that invocations can be chained fluently.
+	 */
 	public AttachmentFactory newAttachment() {
 		if(interrupt) { throw new IllegalStateException("Already constructing a new attachment"); }
 		interrupt = true;
@@ -60,54 +65,99 @@ public class AttachmentFactory {
 		return this;
 	}
 	
+	/**
+	 * Sets the MIME type of the attachment to create.
+	 * @param mime An Internet Media Type or MIME type.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setMime(String mime) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.mime = mime;
 		return this;
 	}
 	
+	/**
+	 * Sets the data content, encoded as {@code Base64}, for the attachment.
+	 * @param rawData A {@code String} of Base64 data.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setData(String rawData) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.data = rawData;
 		return this;
 	}
 	
+	/**
+	 * Sets the name of the attachment file.
+	 * @param fileName A {@code String} representing the original filename.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setName(String fileName) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.name = fileName;
 		return this;
 	}
 	
+	/**
+	 * Sets the unique ID of the attachment within a Bugzilla installation.
+	 * @param id A unique integer identifying the attachment.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setID(int id) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.id = id;
 		return this;
 	}
 	
+	/**
+	 * Sets the unique ID of the parent {@link Bug} for an attachment within a Bugzilla installation.
+	 * @param bugID A unique integer identifying the attachment.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setBugID(int bugID) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.bugID = bugID;
 		return this;
 	}
 	
+	/**
+	 * Sets the summary of the attachment to create.
+	 * @param summary A {@code String} describing the attachment.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setSummary(String summary) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.summary = summary;
 		return this;
 	}
 	
+	/**
+	 * Sets the creator of the attachment being created.
+	 * @param creator A {@code String} representing the email address of the uploader.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setCreator(String creator) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.creator = creator;
 		return this;
 	}
 	
+	/**
+	 * Sets the {@link Date} the attachment was first created on.
+	 * @param created A date describing when the attachment was uploaded.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setCreationDate(Date created) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.created = created;
 		return this;
 	}
 	
+	/**
+	 * Sets the {@link Date} the attachment was last modified.
+	 * @param modified A date describing when the attachment was last changed.
+	 * @return A reference to the current {@link AttachmentFactory}.
+	 */
 	public AttachmentFactory setModifiedDate(Date modified) {
 		if(!interrupt) { throw new IllegalStateException("Need to call newAttachment() first"); }
 		this.modified = modified;
