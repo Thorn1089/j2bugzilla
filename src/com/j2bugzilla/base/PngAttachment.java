a6fe95a0c74d770427786f681ac3aece994ff85f
TomRK1089 <TomRK1089@localhost>
2012-02-27 19:40:25 -0500

    Added commons-codec dependency for decoding Base64'd attachment data; added basic implementation for getting an Image out of a PngAttachment.

--- src/com/j2bugzilla/base/PngAttachment.java
+++ src/com/j2bugzilla/base/PngAttachment.java
@@ -1,5 +1,14 @@
 package com.j2bugzilla.base;
 
+import java.awt.Image;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.ws.commons.util.Base64;
+import org.apache.ws.commons.util.Base64.DecodingException;
+
 /**
  * The {@code PngAttachment} class is a subtype of {@link Attachment} designed to decipher and display
  * PNG files which have been attached to Bugzilla.
@@ -17,6 +26,11 @@ public class PngAttachment extends Attachment {
 	PngAttachment(String data, String name, int id, int bugId) {
 		super(data, name, id, bugId);
 		this.setMIMEType("image/png");
+	}
+	
+	public Image getImage() throws IOException {
+		byte[] data = Base64.decode(this.getRawData());
+		return ImageIO.read(new ByteArrayInputStream(data));
 	}
 
 }
