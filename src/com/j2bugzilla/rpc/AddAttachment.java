072d766606a54b2c842422c83371e18d4b7cffe1
TomRK1089 <TomRK1089@localhost>
2012-02-28 00:09:10 -0500

    Fleshed out and documented class.

--- src/com/j2bugzilla/rpc/AddAttachment.java
+++ src/com/j2bugzilla/rpc/AddAttachment.java
@@ -1,5 +1,6 @@
 package com.j2bugzilla.rpc;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,13 @@ import com.j2bugzilla.base.Bug;
 import com.j2bugzilla.base.BugzillaMethod;
 import com.j2bugzilla.base.Comment;
 
+/**
+ * The {@code AddAttachment} class allows clients to post new attachments to existing {@link Bug Bugs},
+ * with an optional descriptive comment.
+ * 
+ * @author Tom
+ *
+ */
 public class AddAttachment implements BugzillaMethod {
 
 	private static final String METHOD_NAME = "Bug.add_attachment";
@@ -16,6 +24,11 @@ public class AddAttachment implements BugzillaMethod {
 	private Map<Object, Object> params = new HashMap<Object, Object>();
 	private Map<Object, Object> hash = new HashMap<Object, Object>();
 	
+	/**
+	 * Add a new {@link Attachment} to the bug specified by the ID.
+	 * @param attachment An {@code Attachment} object with the file data to upload.
+	 * @param id A unique integer identifying a {@link Bug}.
+	 */
 	public AddAttachment(Attachment attachment, int id) {
 		params.put("ids", id);
 		params.put("data", attachment.getRawData());
@@ -24,17 +37,48 @@ public class AddAttachment implements BugzillaMethod {
 		params.put("content_type", attachment.getMIMEType());
 	}
 	
+	/**
+	 * Add a new {@link Attachment} to the specified {@link Bug}.
+	 * @param attachment An {@code Attachment} object with the file data to upload.
+	 * @param bug The {@code Bug} to attach the file to.
+	 */
 	public AddAttachment(Attachment attachment, Bug bug) {
 		this(attachment, bug.getID());
 	}
 	
+	/**
+	 * Add a new {@link Attachment} to the specified {@link Bug}, with a descriptive comment.
+	 * @param attachment An {@code Attachment} object with the file data to upload.
+	 * @param bug The {@code Bug} to attach the file to.
+	 * @param comment A descriptive {@code String} comment.
+	 */
 	public AddAttachment(Attachment attachment, Bug bug, String comment) {
 		this(attachment, bug);
 		params.put("comment", comment);
 	}
 	
+	/**
+	 * Add a new {@link Attachment} to the specified {@link Bug}, with a descriptive {@link Comment}.
+	 * @param attachment An {@code Attachment} object with the file data to upload.
+	 * @param bug The {@code Bug} to attach the file to.
+	 * @param comment A descriptive {@code Comment}.
+	 */
 	public AddAttachment(Attachment attachment, Bug bug, Comment comment) {
 		this(attachment, bug, comment.getText());
+	}
+	
+	/**
+	 * Returns the ID of the submitted {@link Attachment}.
+	 * @return An integer ID for the newly created attachment.
+	 */
+	public int getID() {
+		if(hash.containsKey("attachments")) {
+			@SuppressWarnings("unchecked")
+			Map<Object, Object> attachmentMap = (Map<Object, Object>)hash.get("attachments");
+			Collection<Object> ids = attachmentMap.values();
+			return (Integer)ids.iterator().next();
+		}
+		return -1;
 	}
 	
 	@Override
