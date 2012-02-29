6b9a1ce7cee430eaa3b7d4777b1f4e2c2dd02c8a
TomRK1089 <TomRK1089@localhost>
2012-02-29 16:46:47 -0500

    Fleshed out test method for LogIn with stubbed connector object. This is pretty nifty!

--- src/test/java/com/j2bugzilla/rpc/TestLogIn.java
+++ src/test/java/com/j2bugzilla/rpc/TestLogIn.java
@@ -1,12 +1,20 @@
 package com.j2bugzilla.rpc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Mockito.*;
 
 import com.j2bugzilla.base.BugzillaConnector;
+import com.j2bugzilla.base.BugzillaException;
 
 import junit.framework.TestCase;
 
@@ -17,8 +25,27 @@ public class TestLogIn extends TestCase {
 	private BugzillaConnector conn;
 	
 	@Test
-	public void testLogIn() {
+	public void testLogIn() throws BugzillaException {
+		LogIn logIn = new LogIn("test@test.org", "password");
 		
+		doAnswer(new Answer<Void>() {
+
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				Object[] args = invocation.getArguments();
+				LogIn rpcMethod = (LogIn)args[0];
+				
+				Map<Object, Object> results = new HashMap<Object, Object>();
+				results.put("id", 1);
+				rpcMethod.setResultMap(results);
+				return null;
+			}
+			
+		}).when(conn).executeMethod(logIn);
+		
+		conn.executeMethod(logIn);
+		
+		assertEquals("Incorrect user ID returned", 1, logIn.getUserID());
 	}
 	
 }
