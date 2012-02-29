f69f4bb411b9c4dff49da05922ae55a595444ef9
TomRK1089 <TomRK1089@localhost>
2012-02-29 16:28:16 -0500

    Added stub for first Mockito test.

---
+++ src/test/java/com/j2bugzilla/rpc/TestLogIn.java
@@ -0,0 +1,24 @@
+package com.j2bugzilla.rpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.j2bugzilla.base.BugzillaConnector;
+
+import junit.framework.TestCase;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestLogIn extends TestCase {
+
+	@Mock
+	private BugzillaConnector conn;
+	
+	@Test
+	public void testLogIn() {
+		
+	}
+	
+}
