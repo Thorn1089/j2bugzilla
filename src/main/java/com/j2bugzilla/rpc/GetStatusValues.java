a57015750fdaa6fc4e018b20a8c037502191baaf
TomRK1089 <TomRK1089@localhost>
2012-03-27 19:38:54 -0400

    Refactored GetStatusValues to defer to base class.

--- src/main/java/com/j2bugzilla/rpc/GetStatusValues.java
+++ src/main/java/com/j2bugzilla/rpc/GetStatusValues.java
@@ -2,11 +2,10 @@ package com.j2bugzilla.rpc;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import com.j2bugzilla.base.BugzillaMethod;
+import com.j2bugzilla.base.Bug;
 
 /**
  * The {@code GetStatusValues} class is a helper method which wraps the {@code Bug.fields} webservice call, to
@@ -17,35 +16,15 @@ import com.j2bugzilla.base.BugzillaMethod;
  * @author Tom
  *
  */
-public class GetStatusValues implements BugzillaMethod {
+public class GetStatusValues extends GetLegalValues {
 
-	private static final String METHOD_NAME = "Bug.fields";
-	
-	private Set<String> legalValues = Collections.emptySet();
-	
+
 	/**
 	 * Returns a {@code Set} of {@code Strings} representing the legal values for the status of a {@link Bug}.
 	 * @return A set of values for a bug's status field.
 	 */
 	public Set<String> getLegalValues() {
-		return Collections.unmodifiableSet(legalValues);
-	}
-	
-	@Override
-	public void setResultMap(Map<Object, Object> hash) {
-		legalValues = new HashSet<String>();
-		
-		Object[] array = (Object[]) hash.get("fields");
-		@SuppressWarnings("unchecked")//Cast to structure defined by webservice
-		Map<Object, Object> fields = (Map<Object, Object>) array[0];
-		
-		Object[] values = (Object[]) fields.get("values");
-		for(Object obj : values) {
-			@SuppressWarnings("unchecked")//Cast to structure defined by webservice
-			Map<Object, Object> map = (Map<Object, Object>) obj;
-			String name = (String) map.get("name");
-			legalValues.add(name);
-		}
+		return super.getLegalValues();
 	}
 
 	@Override
@@ -53,10 +32,5 @@ public class GetStatusValues implements BugzillaMethod {
 		Map<Object, Object> params = new HashMap<Object, Object>();
 		params.put("names", new String[] { "bug_status" });
 		return Collections.unmodifiableMap(params);
-	}
-
-	@Override
-	public String getMethodName() {
-		return METHOD_NAME;
 	}
 }
