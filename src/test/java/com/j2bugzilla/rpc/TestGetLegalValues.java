7187b23e6730ea2a7dac9224091448f0288e9333
TomRK1089 <TomRK1089@localhost>
2012-03-27 20:18:02 -0400

    Added more generic test for GetLegalValues.

---
+++ src/test/java/com/j2bugzilla/rpc/TestGetLegalValues.java
@@ -0,0 +1,73 @@
+package com.j2bugzilla.rpc;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import com.j2bugzilla.base.BugzillaConnector;
+import com.j2bugzilla.base.BugzillaException;
+import com.j2bugzilla.rpc.GetLegalValues.Fields;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestGetLegalValues {
+
+	@Mock
+	private BugzillaConnector conn;
+	
+	@Test
+	public void testGetLegalValues() throws BugzillaException {
+		GetLegalValues getLegalVals = new GetLegalValues(Fields.SEVERITY);
+		
+		doAnswer(new Answer<Void>() {
+
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				GetLegalValues rpcMethod = (GetLegalValues)invocation.getArguments()[0];
+				
+				Map<Object, Object> hash = new HashMap<Object, Object>();
+				
+				Object[] arr = new Object[1];
+				Map<Object, Object> valMap = new HashMap<Object, Object>();
+				arr[0] = valMap;
+				
+				
+				Map<String, String> val1 = new HashMap<String, String>();
+				val1.put("name", "blocker");
+				Map<String, String> val2 = new HashMap<String, String>();
+				val2.put("name", "major");
+				Map<String, String> val3 = new HashMap<String, String>();
+				val3.put("name", "minor");
+				
+				Object[] values = new Object[] { val1, val2, val3 };
+				valMap.put("values", values);
+								
+				hash.put("fields", arr);
+				
+				rpcMethod.setResultMap(hash);
+				
+				return null;
+			}
+			
+		}).when(conn).executeMethod(getLegalVals);
+		
+		conn.executeMethod(getLegalVals);
+		
+		Set<String> severities = getLegalVals.getLegalValues();
+		
+		assertTrue("Severity missing", severities.contains("blocker"));
+		assertTrue("Severity missing", severities.contains("major"));
+		assertTrue("Severity missing", severities.contains("minor"));
+	}
+
+}
