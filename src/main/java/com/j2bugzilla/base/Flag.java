b0d960d9f6122c9ff738294cad8ab59044959472
TomRK1089 <TomRK1089@localhost>
2012-10-10 18:38:38 -0400

    Fleshed out the Flag class.

--- src/main/java/com/j2bugzilla/base/Flag.java
+++ src/main/java/com/j2bugzilla/base/Flag.java
@@ -5,14 +5,63 @@ package com.j2bugzilla.base;
  * of several states: {@code '+', '-', '?' or ' '}. The meaning of a {@code Flag} is context-dependent.
  * 
  * @author tom
- *
  */
 public class Flag {
 
+	/**
+	 * The {@code Status} enum represents the current state of a {@link Flag}. It can represent one of four values.
+	 * @author tom
+	 */
+	public enum Status { 
+		/**
+		 * The {@code UNSET} {@link Status} represents a {@link Flag} which has not been toggled. It is represented by ' '.
+		 */
+		UNSET, 
+		
+		/**
+		 * The {@code POSITIVE} {@link Status} represents a {@link Flag} which has been toggled to '+'.
+		 */
+		POSITIVE, 
+		
+		/**
+		 * The {@code NEGATIVE} {@link Status} represents a {@link Flag} which has been toggled to '-'.
+		 */
+		NEGATIVE, 
+		
+		/**
+		 * The {@code UNKNOWN} {@link Status} represents a {@link Flag} which has been toggled to '?'.
+		 */
+		UNKNOWN 
+	}
+	
 	private final String name;
 	
-	Flag(String name) {
+	private final Status status;
+	
+	/**
+	 * Creates a new {@link Flag} with the given name and status.
+	 * @param name A unique name describing this {@code Flag}.
+	 * @param status A {@link Status} describing the state of this {@code Flag}.
+	 */
+	Flag(String name, Status status) {
 		this.name = name;
+		this.status = status;
+	}
+	
+	/**
+	 * Returns the unique name for this {@link Flag}.
+	 * @return A {@code String} representing the name.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns the current state of this {@link Flag}.
+	 * @return A {@link Status} enum representing the {@code Flag} state.
+	 */
+	public Status getStatus() {
+		return status;
 	}
 	
 }
