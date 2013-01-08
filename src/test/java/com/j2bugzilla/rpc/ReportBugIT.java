5b617cc1d618ffd5c3421224ed690ccc1f63c883
Tom Golden <TomRK1089@gmail.com>
2013-01-07 20:13:50 -0500

    Parameterized integration tests.

--- src/test/java/com/j2bugzilla/rpc/ReportBugIT.java
+++ src/test/java/com/j2bugzilla/rpc/ReportBugIT.java
@@ -1,10 +1,16 @@
 package com.j2bugzilla.rpc;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 
-import static org.hamcrest.CoreMatchers.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.j2bugzilla.base.Bug;
 import com.j2bugzilla.base.BugFactory;
@@ -12,12 +18,31 @@ import com.j2bugzilla.base.BugzillaConnector;
 import com.j2bugzilla.base.BugzillaException;
 import com.j2bugzilla.base.ConnectionException;
 
+@RunWith(Parameterized.class)
 public class ReportBugIT {
 
+	@Parameters
+	public static List<Object[]> getUrls() {
+		List<Object[]> urls = new ArrayList<Object[]>();
+		
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-3.6-branch/"});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.0-branch/"});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.2-branch/"});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.4-branch/"});
+		
+		return urls;
+	}
+	
+	private String url;
+	
+	public ReportBugIT(String url) {
+		this.url = url;
+	}
+	
 	@Test
 	public void testGetID() throws ConnectionException, BugzillaException {
 		BugzillaConnector conn = new BugzillaConnector();
-		conn.connectTo("https://landfill.bugzilla.org/bugzilla-4.4-branch");
+		conn.connectTo(url);
 		
 		LogIn logIn = new LogIn("j2bugzilla@gmail.com", "javabugzilla");
 		conn.executeMethod(logIn);
