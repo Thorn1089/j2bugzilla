91d6209a9466a29e8f4df2b2f691883a971af7d9
Tom Golden <TomRK1089@gmail.com>
2013-01-07 20:13:50 -0500

    Parameterized integration tests.

--- src/test/java/com/j2bugzilla/rpc/LogInIT.java
+++ src/test/java/com/j2bugzilla/rpc/LogInIT.java
@@ -2,20 +2,45 @@ package com.j2bugzilla.rpc;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.j2bugzilla.base.BugzillaConnector;
 import com.j2bugzilla.base.BugzillaException;
 import com.j2bugzilla.base.ConnectionException;
 
+@RunWith(Parameterized.class)
 public class LogInIT {
 
+	@Parameters
+	public static List<Object[]> getUrls() {
+		List<Object[]> urls = new ArrayList<Object[]>();
+		
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-3.6-branch/"});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.0-branch/"});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.2-branch/"});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.4-branch/"});
+		
+		return urls;
+	}
+	
+	private String url;
+	
+	public LogInIT(String url) {
+		this.url = url;
+	}
+	
 	@Test
 	public void testGetUserID() throws ConnectionException, BugzillaException {
 		BugzillaConnector conn = new BugzillaConnector();
-		conn.connectTo("https://landfill.bugzilla.org/bugzilla-4.4-branch");
+		conn.connectTo(url);
 		
 		LogIn logIn = new LogIn("j2bugzilla@gmail.com", "javabugzilla");
 		conn.executeMethod(logIn);
--- src/test/java/com/j2bugzilla/rpc/UpdateBugIT.java
+++ src/test/java/com/j2bugzilla/rpc/UpdateBugIT.java
@@ -1,23 +1,51 @@
 package com.j2bugzilla.rpc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.j2bugzilla.base.Bug;
 import com.j2bugzilla.base.BugzillaConnector;
 import com.j2bugzilla.base.BugzillaException;
 import com.j2bugzilla.base.ConnectionException;
 
+@RunWith(Parameterized.class)
 public class UpdateBugIT {
 
+	@Parameters
+	public static List<Object[]> getUrls() {
+		List<Object[]> urls = new ArrayList<Object[]>();
+		
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-3.6-branch/", 1058});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.0-branch/", 1058});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.2-branch/", 1058});
+		urls.add(new Object[]{"https://landfill.bugzilla.org/bugzilla-4.4-branch/", 1058});
+		
+		return urls;
+	}
+	
+	private String url;
+	
+	private int id;
+	
+	public UpdateBugIT(String url, int id) {
+		this.url = url;
+		this.id = id;
+	}
+	
 	@Test
 	public void test() throws ConnectionException, BugzillaException {
 		BugzillaConnector conn = new BugzillaConnector();
-		conn.connectTo("https://landfill.bugzilla.org/bugzilla-4.4-branch");
+		conn.connectTo(url);
 		
 		LogIn logIn = new LogIn("j2bugzilla@gmail.com", "javabugzilla");
 		conn.executeMethod(logIn);
 		
-		GetBug get = new GetBug(1058);
+		GetBug get = new GetBug(id);
 		conn.executeMethod(get);
 		
 		Bug bug = get.getBug();
